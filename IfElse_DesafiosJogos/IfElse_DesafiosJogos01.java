b137130286020b4e38c40ce1ce9e1b2bdf8d7839
GabrielleGoulart <gabriellegoulart48@gmail.com>
2025-01-30 16:26:48 -0300

    Add files via upload

---
+++ IfElse_DesafiosJogos/IfElse_DesafiosJogos01.java
@@ -0,0 +1,25 @@
+package IfElse_DesafiosJogos;
+import java.util.Scanner;
+
+public class IfElse_DesafiosJogos01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro numero: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        int num2 = sc.nextInt();
+
+        if (num1 > 0 && num2 > 0) {
+            System.out.println("ambos numeros são positivos");
+        } else if (num1 < 0 && num2 < 0) {
+            System.out.println("ambos numeros são negativos");
+        } else{
+            System.out.println("os numeros tem sinais diferentes");
+
+            sc.close();
+        }
+    }
+}
+
---
+++ IfElse_DesafiosJogos/IfElse_DesafiosJogos02.java
@@ -0,0 +1,23 @@
+package IfElse_DesafiosJogos;
+import java.util.Scanner;
+
+public class IfElse_DesafiosJogos02 {
+     public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("Digite o primeiro numero: ");
+            int num1 = sc.nextInt();
+
+            System.out.print("Digite o segundo numero: ");
+            int num2 = sc.nextInt();
+
+            if (num1 > 2 * num2) {
+                System.out.println("o numero " +  num1 +" é maior que o dobro de " + num2);
+            }else {
+                System.out.println("o numero " + num1 +" NÃO é maior que o dobro de " + num2);
+            }
+            sc.close();
+        }
+
+    }
+
